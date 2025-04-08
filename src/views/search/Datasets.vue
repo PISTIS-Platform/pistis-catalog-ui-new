13adda32c4ba17e9355c8c527df3b6e33019c292
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2025-04-08 08:07:17 +0200

    Add layout for search page

--- src/views/search/Datasets.vue
+++ src/views/search/Datasets.vue
@@ -1,4 +1,29 @@
 <template>
+  <div
+      name="content"
+      class="flex flex-col overflow-x-auto"
+  >
+    <section class="sm:hidden">
+      <slot name="hamburger">
+        hamburger
+      </slot>
+    </section>
+    <div class="mt-[15px] flex-none px-6 py-[1.875rem]">
+      <slot name="top">
+        <div>A</div>
+      </slot>
+    </div>
+    <div class="flex-1">
+      <slot>
+        <div class="h-[1000px]">
+          C
+        </div>
+        <div class="h-[1000px]">
+          D
+        </div>
+      </slot>
+    </div>
+  </div>
   <div class="flex flex-col gap-2">
     <template v-if="!isLoading && !isFetching">
       <DataInfoCard
@@ -13,7 +38,7 @@
     </template>
     <template v-else>
       <div v-for="i in itemsCount.value" :key="i" class="size-full flex flex-col gap-6 text-content animate-pulse bg-slate-200">
-        <div class="flex flex-wrap gap-2 h-36" />
+        <div class="flex flex-wrap gap-2 h-64" />
       </div>
     </template>
   </div>
@@ -26,14 +51,6 @@ import DataInfoCard from '@/components/base/data-info-box/DataInfoCard.vue'
 import { useDatasetSearchView } from '@/composables/useDatasetsSearchView'
 import { useSearchParams } from '@/composables/useSearchParams'
 import { useSelectedFacets } from '@/composables/useSelectedFacets'
-
-// const datasets = [{
-//   id: 1,
-//   title: "Mock Dataset",
-//   description: "Mock Dataset description !!!",
-//   formats: [],
-//   summary: "Mock Dataset",
-// }];
 
 const props = withDefaults(
     defineProps<{
