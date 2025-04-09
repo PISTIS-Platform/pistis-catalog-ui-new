9d9a08440155fc04afe826eb65cb7cdc9c4e8ffd
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2025-04-09 15:12:56 +0200

    factor out search items

--- src/views/search/datasets/Datasets.vue
+++ src/views/search/datasets/Datasets.vue
@@ -70,32 +70,13 @@
                 {{ formattedDatasetResultCount }}
             </SearchInfoPanel>
           </div>
-          <div class="flex flex-col gap-2">
-            <template v-if="!isLoading && !isFetching">
-              <DataInfoCard
-                  v-for="dataset in datasets"
-                  :key="dataset.id"
-                  :to="{ name: 'dataset-details', params: { datasetId: dataset.id } }"
-                  :title="dataset.title"
-                  :description="dataset.description"
-                  :file-formats="dataset.formats"
-                  :properties="dataset.summary"
-              />
-            </template>
-            <template v-else>
-              <div v-for="i in itemsCount.value" :key="i" class="size-full flex flex-col gap-6 text-content animate-pulse bg-slate-200">
-                <div class="flex flex-wrap gap-2 h-64" />
-              </div>
-            </template>
-          </div>
-          <div class="grid w-full place-content-center">
-            <Paginator
-                v-model:first="searchParams.queryParams.page.value"
-                class="rounded"
-                :rows="1"
-                :total-records="getSearchResultsPagesCount"
-            />
-          </div>
+          <search-items
+              :datasets="datasets"
+              :getSearchResultsPagesCount="getSearchResultsPagesCount"
+              :isLoading="isLoading"
+              :isFetching="isFetching"
+              :showOnlyPublic="showOnlyPublic"
+          />
         </section>
       </div>
     </div>
@@ -105,7 +86,6 @@
 <script setup lang="ts">
 import { useDcatApSearch } from '@/sdk'
 import { computed, toRef } from 'vue'
-import DataInfoCard from '@/components/base/data-info-box/DataInfoCard.vue'
 import { useDatasetSearchView } from '@/composables/useDatasetsSearchView'
 import { useSearchParams } from '@/composables/useSearchParams'
 import { useSelectedFacets } from '@/composables/useSelectedFacets'
@@ -113,10 +93,9 @@ import KButton from '@/components/base/button/KButton.vue'
 import FacetSidebar from '@/components/facet-sidebar/FacetSidebar.vue'
 import SelectedFacetsOverview from '@/components/selected-facets-overview/SelectedFacetsOverview.vue'
 import SearchInfoPanel from '@/components/base/search-info-panel/SearchInfoPanel.vue'
-import Paginator from 'primevue/paginator'
 import Sidebar from 'primevue/sidebar'
 import SearchBar from "@/views/search/datasets/SearchBar.vue";
-// import SearchInput from "@/components/search-input/SearchInput.vue"
+import SearchItems from "@/views/search/datasets/SearchItems.vue";
 
 const searchInput = defineModel<string>('searchInput', { required: true })
 const hvdModel = defineModel<boolean>('hvd', { required: true })
