d86a4bab4d4bd87757c02b1c75802579b1ffb1f0
Martin Koerwien <martin.koerwien@fokus.fraunhofer.de>
2025-05-13 14:21:01 +0200

    fix further linting issues

--- src/components/header-footer/Header.vue
+++ src/components/header-footer/Header.vue
@@ -9,13 +9,13 @@ const visible = ref(false)
 
 <template>
   <header class=" border-gray-200 bg-white">
-    <div class="flex justify-between gap-3 lg:container lg:px-8 py-5 items-center px-8 mx-auto">
+    <div class="mx-auto flex items-center justify-between gap-3 px-8 py-5 lg:container lg:px-8">
       <RouterLink class="block" to="/">
         <div class="brand">
           <img
-              src="../../assets/images/piveau-logo.svg"
-              alt="open.bydata"
-              class="block h-8"
+            src="../../assets/images/piveau-logo.svg"
+            alt="open.bydata"
+            class="block h-8"
           >
           <!-- <span class="ml-5">Piveau</span> -->
         </div>
@@ -25,18 +25,18 @@ const visible = ref(false)
           <NavigationBar />
         </div>
         <div class="authentication-buttons">
-          <div class="card flex justify-content-center">
+          <div class="card justify-content-center flex">
             <Sidebar
-                v-model:visible="visible"
-                header="Sidebar"
-                position="full"
+              v-model:visible="visible"
+              header="Sidebar"
+              position="full"
             >
               <template #header>
-                <div class="flex items-center justify-center px-2 h-12">
+                <div class="flex h-12 items-center justify-center px-2">
                   <img
-                      src="/logo.png"
-                      alt="open.bydata"
-                      class="h-12"
+                    src="/logo.png"
+                    alt="open.bydata"
+                    class="h-12"
                   >
                 </div>
               </template>
@@ -45,21 +45,21 @@ const visible = ref(false)
               </div>
             </Sidebar>
             <KButton
-                severity="contrast"
-                class="block md:hidden"
-                icon="icon-[ph--list]"
-                @click="visible = true"
+              severity="contrast"
+              class="block md:hidden"
+              icon="icon-[ph--list]"
+              @click="visible = true"
             />
           </div>
         </div>
       </div>
-        <div class="flex items-center gap-2">
-        <a target="_blank" href="https://doc.piveau.eu/general/introduction/" class="mr-4 font-light flex items-center">Docs <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="ml-2 bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
-  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
-</svg></a>
-        <a  target="_blank" href="https://gitlab.com/piveau/" class="font-light flex items-center"><img src="../../assets/images/icons8-gitlab-48.png" alt="Gitlab" class="h-4 w-4 mr-1">GitLab </a>
-       </div>
+      <div class="flex items-center gap-2">
+        <a target="_blank" href="https://doc.piveau.eu/general/introduction/" class="mr-4 flex items-center font-light">Docs <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-box-arrow-up-right ml-2" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5" />
+          <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z" />
+        </svg></a>
+        <a target="_blank" href="https://gitlab.com/piveau/" class="flex items-center font-light"><img src="../../assets/images/icons8-gitlab-48.png" alt="Gitlab" class="mr-1 size-4">GitLab </a>
+      </div>
     </div>
   </header>
 </template>
