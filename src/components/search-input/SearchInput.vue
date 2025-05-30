3087fc79e40d68d9d69704b4ff9a56db1c042b8b
Bachmann <lisa.bachmann@fokus.fraunhofer.de>
2025-05-30 12:15:04 +0200

    replace InputText

--- src/components/search-input/SearchInput.vue
+++ src/components/search-input/SearchInput.vue
@@ -13,18 +13,16 @@ const searchValue = defineModel<string>()
 </script>
 
 <template>
-    <InputText
-      v-model="searchValue"
-      :placeholder="props.placeholder"
-      size="small"
-      class="custom-input"
-    />
-
+  <input
+    v-model="searchValue"
+    :placeholder="props.placeholder"
+    size="small"
+    class="custom-input text-surface-800 dark:text-surface-200 bg-surface-0 dark:bg-surface-900 border focus:outline-hidden" />
 </template>
 
 <style scoped>
 .custom-input {
-  border: 0.3px solid #dedede ;
+  border: 0.3px solid #dedede;
   border-radius: 100px !important;
   padding-left: 1rem;
 }
