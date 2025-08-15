4e2676e4558ce214e7cd7338a94ee219c1833a23
lis02409 <lis02409@vpn3d161.fokus.fraunhofer.de>
2025-08-15 11:39:00 +0200

    translate text monetization

--- src/components/monetization/Metadata.vue
+++ src/components/monetization/Metadata.vue
@@ -1,17 +1,17 @@
 <template>
   <div v-if="isObject(data)">
-    <p><strong>Transferable:</strong> {{ data.transferable }}</p>
+    <p><strong>{{ $t('monetization.transferable') }}:</strong> {{ data.transferable }}</p>
 
-    <p><strong>Type:</strong> {{ data.type }}</p>
+    <p><strong>{{ $t('monetization.type') }}:</strong> {{ data.type }}</p>
 
-    <p><strong>Exclusive:</strong> {{ data.is_exclusive ? 'Yes' : 'No' }}</p>
+    <p><strong>{{ $t('monetization.exclusive') }}:</strong> {{ data.is_exclusive ? 'Yes' : 'No' }}</p>
 
-    <p><strong>Termination Date:</strong> {{ data.termDate }}</p>
+    <p><strong>{{ $t('monetization.termination-date') }}:</strong> {{ data.termDate }}</p>
 
-    <p><strong>Price:</strong> {{ data.price }} €</p>
+    <p><strong>{{ $t('monetization.price') }}:</strong> {{ data.price }} €</p>
 
     <div v-if="data.downloads && data.downloads.length">
-      <strong>Downloads:</strong>
+      <strong>{{ $t('monetization.downloads') }}:</strong>
       <ul>
         <li v-for="(dl, index) in data.downloads" :key="index">
           {{ dl.frequency }}: {{ dl.downloads }}
@@ -20,7 +20,7 @@
     </div>
 
     <div v-if="data.personal_data_terms && data.personal_data_terms.length">
-      <strong>Personal Data Terms:</strong>
+      <strong>{{ $t('monetization.personal-data-terms') }}:</strong>
       <ul>
         <li v-for="(term, index) in data.personal_data_terms" :key="index">
           <span v-if="term.contains_personal_data">{{ term.personal_data_terms }}</span>
@@ -30,7 +30,7 @@
     </div>
 
     <div v-if="data.license">
-      <strong>License:</strong>
+      <strong>{{ $t('monetization.license') }}:</strong>
       <p class="p-0!">
         <a :href="data.license.la_url" target="_blank" rel="noopener noreferrer">{{ data.license.label }}</a><br />
         {{ data.license.description }}
@@ -41,6 +41,9 @@
 
 <script setup>
 import { defineProps } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const t = useI18n
 
 const props = defineProps({
   data: {
--- src/components/monetization/MonetizationView.vue
+++ src/components/monetization/MonetizationView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <Typography variant="by-heading-4">Monetization</Typography>
+    <Typography variant="by-heading-4"> {{ $t('monetization.header') }} </Typography>
     <Metadata :data="mockData.monetization[0]" />
   </div>
 </template>
@@ -10,8 +10,10 @@ import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import Metadata from './Metadata.vue'
 import mockData from './mock-data.json'
+import { useI18n } from 'vue-i18n'
 
-//commented out for dev with mockData
+const t = useI18n
+
 // const route = useRoute();
 
 // let datasetId = route.params.ds_id.toString();
@@ -30,20 +32,20 @@ import mockData from './mock-data.json'
 
 //     metadata.value = data
 
-//     filteredData.value.offer = metadata.value.result.offer;
-//     filteredData.value.insights_result = metadata.value.result.insights_result;
-//     filteredData.value.monetization = metadata.value.result.monetization
-//         .filter(item => !item.visible)
-//         .map(item => ({
-//             "Transferable": item.transferable,
-//             "Downloads": item.downloads,
-//             "Type": item.type,
-//             "Exclusive": item.is_exclusive,
-//             "Termination Date": item.termDate,
-//             "Price": item.price,
-//             "Personal Data Terms": item.personal_data_terms,
-//             "License": item.license ? item.license.description : null
-//         }));
+    // filteredData.value.offer = metadata.value.result.offer;
+    // filteredData.value.insights_result = metadata.value.result.insights_result;
+    // filteredData.value.monetization = metadata.value.result.monetization
+    //     .filter(item => !item.visible)
+    //     .map(item => ({
+    //         "Transferable": item.transferable,
+    //         "Downloads": item.downloads,
+    //         "Type": item.type,
+    //         "Exclusive": item.is_exclusive,
+    //         "Termination Date": item.termDate,
+    //         "Price": item.price,
+    //         "Personal Data Terms": item.personal_data_terms,
+    //         "License": item.license ? item.license.description : null
+    //     }));
 // }
 
 // onMounted(() => {
